7aa55adc8b1ef75c4ceebb57c89f6b84163273cc
rupam office <rupamdhara.1579@gmail.com>
2024-07-22 18:15:03 +0530

    Updates

--- bandhan/settings.py
+++ bandhan/settings.py
@@ -117,13 +117,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-
+import os
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR, 'static',   # Adjust path as necessary
-    
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/uploads/'
 MEDIA_ROOT =  BASE_DIR / 'uploads'
